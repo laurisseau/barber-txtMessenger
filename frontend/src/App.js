820b6363264057f3f7d52450f2b5703ec85d6e71
laurisseau Joseph <reso0208@gmail.com>
2023-03-08 15:20:52 -0500

    CheckBox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,42 +2,39 @@ import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const test = [
+  { id: 1, value: 'banana'},
+  { id: 2, value: 'apple'},
+  { id: 3, value: 'mango'},
+  { id: 4, value: 'grap'},
+];
+
 function App() {
+  //const [isCheckAll, setIsCheckAll] = useState(false);
+  //const [isCheck, setIsCheck] = useState([]);
+  const [list, setList] = useState([]);
 
-  const [isCheckAll, setIsCheckAll] = useState(false);
-  const [isCheck, setIsCheck] = useState([]);
-  const [list, setList] = useState([
-    { id: 1, value: "banana", isChecked: false },
-    { id: 2, value: "apple", isChecked: false },
-    { id: 3, value: "mango", isChecked: false },
-    { id: 4, value: "grap", isChecked: false }
-  ]);
+  useEffect(() => {
+    setList(test);
+  }, []);
 
-  const handleSelectAll = e => {
-    setIsCheckAll(!isCheckAll);
-    
-    setIsCheck(list.map(li => `${li.id}`));
-    
-    if (isCheckAll) {
-      setIsCheck([]);
+  const handleChange = (e) => {
+    const { name, checked } = e.target;
+    if (name === 'allSelect') {
+      let tempList = list.map((li) => {
+        return { ...li, isChecked: checked };
+      });
+      setList(tempList);
+    } else {
+      let tempList = list.map((li) =>
+        li.value === name ? { ...li, isChecked: checked } : li
+      );
+      setList(tempList);
     }
   };
-  console.log(isCheckAll)
-  const handleClick = e => {
-    const { id, checked } = e.target;
-
-    setIsCheck([...isCheck, id]);
-
-    if (!checked) {
-      setIsCheck(isCheck.filter(item => item !== id));
-    }
-  };
-
-console.log(isCheck)
-
 
   return (
     <Container>
@@ -46,16 +43,15 @@ console.log(isCheck)
           <div className="d-flex justify-content-between mb-3 mt-3">
             <button className="ps-5 pe-5 button">+</button>
             <div>
-            <input
-        type="checkbox"
-        name="selectAll"
-        id="selectAll"
-        onChange={handleSelectAll}
-        checked={isCheckAll}
-                  />
-                  Select All
-                  </div>
-          </div> 
+              <input
+                type="checkbox"
+                className="form-check-input"
+                name="allSelect"
+                onChange={handleChange}
+              />
+              Select All
+            </div>
+          </div>
           <Table className="table border clients" responsive>
             <thead>
               <tr>
@@ -74,22 +70,22 @@ console.log(isCheck)
               </tr>
             </thead>
             <tbody>
-              {list.map((items, id)=>(
-              <tr key={id}>
-                <th>
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    id={items.id}
-                    onChange={handleClick}
-                  />
-                </th>
-                <td>{items.value}</td>
-                <td>123</td>
-                <td>del</td>
-              </tr>
-              ))
-              }
+              {list.map((items, id) => (
+                <tr key={id}>
+                  <th>
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      name={items.value}
+                      checked={items?.isChecked || false}
+                      onChange={handleChange}
+                    />
+                  </th>
+                  <td>{items.value}</td>
+                  <td>123</td>
+                  <td>del</td>
+                </tr>
+              ))}
             </tbody>
           </Table>
         </Col>
@@ -100,4 +96,3 @@ console.log(isCheck)
 
 export default App;
 
-//className="border rounded"
